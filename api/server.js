6eb41d3f1780c716b95f02bcdfc86d658a078e4c
Thouseef <thouseefhamza.codes@gmail.com>
2024-12-03 21:39:50 +0530

    Changes to the cors

--- api/server.js
+++ api/server.js
@@ -24,7 +24,7 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://freelance-market-place.vercel.app/", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
