af25aa8ebef60d733431e21481d2ae33e6dafa5d
Thouseef <thouseefhamza.codes@gmail.com>
2024-12-04 12:34:09 +0530

    Allow all origins

--- api/server.js
+++ api/server.js
@@ -25,13 +25,10 @@ const connect = async () => {
 };
 app.use(
   cors({
-    origin: "https://freelance-market-place-shp4.vercel.app",
+    origin: "*",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.options("*", cors());
 app.use(express.json());
 app.use(cookieParser());
 
