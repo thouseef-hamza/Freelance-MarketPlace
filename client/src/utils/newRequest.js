081f3cdf37b1d8069465de79fdd40f4c10c05ccb
Thouseef Hamza TP <119723781+thouseef-hamza@users.noreply.github.com>
2024-12-10 18:23:15 +0530

    Update newRequest.js

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //backend port number
 const newRequest=axios.create({
-    baseURL:"https://freelance-market-place.vercel.app/api/",
+    baseURL:"https://lionfish-app-omgjk.ondigitalocean.app/api/",
     withCredentials:true,
 })
 
